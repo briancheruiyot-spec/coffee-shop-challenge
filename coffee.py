05a6dda444df417621e6bda47f9677ddd6cbbb70
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-18 20:45:30 +0300

    Updated coffee.py

---
+++ coffee.py
@@ -0,0 +1,11 @@
+class Coffee:
+  def __init__(self, name):
+    if isinstance(name, str) and len(name) >= 3:
+      self._name = name
+    else:
+      raise ValueError("Coffee name must be a string of at least 3 characters")
+    self._order = []
+
+    @property
+    def name(self):
+      return self._name

c9bc2d2dd5bd3f2df9efdfaa4936f8592815928b
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-18 19:29:22 +0300

    Updated customer.py

---
+++ customer.py
@@ -0,0 +1,15 @@
+class Customer:
+  def __init__(self, name):
+    self.name = name
+    self._order = []
+
+  @property
+  def name(self):
+    return self._name
+  
+  @name.setter
+  def name(self, value):
+    if isinstance(value, str) and 1 <= len(value) <=15:
+      self._name = value
+    else:
+      raise ValueError("Name must be a string 1-15 characters long.")

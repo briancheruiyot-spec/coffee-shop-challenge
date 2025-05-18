ad781ca79b1a1c90a9bf8cee2e3e9ca0b3375e74
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-18 21:43:06 +0300

    Updated customer.py

--- customer.py
+++ customer.py
@@ -1,6 +1,3 @@
-from order import Order
-from coffee import Coffee
-
 class Customer:
   def __init__(self, name):
     self.name = name
@@ -16,3 +13,15 @@ class Customer:
       self._name = value
     else:
       raise ValueError("Name must be a string 1-15 characters long.")
+    
+  def orders(self):
+    return self._orders
+  
+  def coffees(self):
+    return list(set(order.coffee for order in self._orders))
+  
+  def create_order(self, coffee, price):
+    from order import Order
+    new_order = Order(self, coffee, price)
+    self._order.append(new_order)
+    return new_order
